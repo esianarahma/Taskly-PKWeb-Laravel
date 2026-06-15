086fc552c1d27a7a5a61196efa967732f4f8ed21
esianarahma <esianarahma15@gmail.com>
2026-06-15 16:59:31 +0700

    feat: enable email verification alongside password reset

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,11 +9,17 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application services.
+     */
     public function register(): void
     {
         //
     }
 
+    /**
+     * Bootstrap any application services.
+     */
     public function boot(): void
     {
         Event::listen(Registered::class, SendEmailVerificationNotification::class);
